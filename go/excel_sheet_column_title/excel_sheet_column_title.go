acc50e13a849e184014d23047c1061a837a64243
Rizky <--replace-all>
2023-02-21 14:18:43 +0700

    unsolved solution for excel sheet column title

--- go/excel_sheet_column_title/excel_sheet_column_title.go
+++ go/excel_sheet_column_title/excel_sheet_column_title.go
@@ -2,34 +2,31 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	fmt.Println(convertToTitle(1))
-	// fmt.Println(convertToTitle(28))
-	// fmt.Println(convertToTitle(701))
-	// fmt.Println(convertToTitle(2759))
+	fmt.Println(convertToTitle(28))
+	fmt.Println(convertToTitle(701))
+	fmt.Println(convertToTitle(2759))
 }
 
 func convertToTitle(columnNumber int) string {
 	res := ""
 	for columnNumber > 0 {
-		if columnNumber%26 > 0 {
-			res = string(rune((columnNumber%26)+64)) + res
-			columnNumber -= columnNumber % 26
-		} else if columnNumber%26 == 0 {
+		mod := columnNumber % 26
+		if mod > 0 {
+			res = string(rune((mod)+64)) + res
+			columnNumber -= mod
+		} else if mod == 0 {
 			div := columnNumber / 26
 			if div > 26 {
-				diff := div % 26
-				res = string(rune(diff+64)) + res
-				columnNumber = div - diff
+				columnNumber = div
 			} else {
 				res = string(rune((columnNumber/26)+64)) + res
 				columnNumber -= columnNumber
 			}
 		}
-		time.Sleep(1 * time.Second)
 	}
 	return res
 }
