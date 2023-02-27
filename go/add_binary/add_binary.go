bc538a7a6176fd956923dff74d83f50fb9edbfbe
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-02-27 10:59:04 +0700

    add solved solution for add binary

--- go/add_binary/add_binary.go
+++ go/add_binary/add_binary.go
@@ -2,56 +2,47 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	addBinary("11", "1")
+	// fmt.Println(addBinary("11", "1"))
+	fmt.Println(addBinary("1010", "1011"))
 }
 
 func addBinary(a string, b string) string {
-	i := 0
-	if len(a) > len(b) {
-		i = len(a)
-	} else {
-		i = len(b)
+	// 1 :: 49
+	i := len(a) - 1
+	j := len(b) - 1
+	add := 0
+
+	var str string
+	for add > 0 || i >= 0 || j >= 0 {
+		res := add
+		if i >= 0 {
+			if a[i] == 49 {
+				res++
+			}
+			i--
+		}
+
+		if j >= 0 {
+			if b[j] == 49 {
+				res++
+			}
+			j--
+		}
+
+		add = 0
+		if res >= 2 {
+			add = 1
+		}
+
+		r := "0"
+		if res%2 == 1 {
+			r = "1"
+		}
+
+		str = r + str
 	}
-
-	r := ""
-	d := 0
-	for j := i - 1; j >= 0; j-- {
-		x := 0
-
-		if j < len(a) {
-			fmt.Printf("A :: %v\n", string(a[j]))
-		}
-
-		if j < len(a) && string(a[j]) == "1" {
-			x = 1
-		}
-
-		y := 0
-
-		if j < len(b) {
-			fmt.Printf("B :: %v\n", string(b[j]))
-		}
-
-		if j < len(b) && string(b[j]) == "1" {
-			y = 1
-		}
-
-		rr := (x + y + d)
-		fmt.Printf("X :: %v\n", rr)
-		if rr == 2 {
-			r = r + "0"
-			d = 1
-		} else {
-			r = r + strconv.Itoa(x+y)
-			d = 0
-		}
-	}
-
-	fmt.Println(r)
-
-	return ""
+	return str
 }
