2675497681961aef27f31d138e8373e2ab73a31e
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-02-27 09:36:08 +0700

    add solved solution for plus one

--- go/plus_one/plus_one.go
+++ go/plus_one/plus_one.go
@@ -6,26 +6,25 @@ func main() {
 	fmt.Println(plusOne([]int{1, 2, 3}))
 	fmt.Println(plusOne([]int{4, 3, 2, 1}))
 	fmt.Println(plusOne([]int{9}))
+	fmt.Println(plusOne([]int{1, 1, 9}))
+	fmt.Println(plusOne([]int{9, 9}))
 }
 
 func plusOne(digits []int) []int {
-	res := 1
-	y := 1
 	for i := len(digits) - 1; i >= 0; i-- {
-		res += digits[i] * y
-		y *= 10
+		if i == len(digits)-1 {
+			digits[i] = digits[i] + 1
+		}
+
+		if digits[i] > 9 {
+			if i > 0 {
+				digits[i-1] = digits[i-1] + 1
+				digits[i] = digits[i] - 10
+			} else {
+				digits = append(digits, digits[i]-10)
+				digits[i] = 1
+			}
+		}
 	}
-
-	y /= 10
-
-	arr := make([]int, len(digits)+1)
-	length := 0
-	for res%10 != 0 {
-		arr[length] = res / y
-		res %= y
-		y /= 10
-		length++
-	}
-
-	return arr
+	return digits
 }
