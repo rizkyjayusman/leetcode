2edb1d3b5f4aed1221d8d577751e773e6206382c
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-03-09 15:49:30 +0700

    add unsolved solution for sum of all odd length sub arrays

---
+++ go/sum_of_all_odd_length_subarrays/sum_of_all_odd_length_subarrays.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(sumOddLengthSubarrays([]int{1, 4, 2, 5, 3}))
+	// fmt.Println(sumOddLengthSubarrays([]int{1,2}))
+	// fmt.Println(sumOddLengthSubarrays([]int{10,11,12}))
+}
+
+func sumOddLengthSubarrays(arr []int) int {
+	// TODO :: !!!!!oqijr0328jfeiwfwkoie
+}
