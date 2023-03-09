6b8f0202a666db858ef19a044df23d33e96d842a
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-03-09 07:33:16 +0700

    add solved solution for minimum sum of four digit number after splitting digits

--- go/minimum_sum_of_four_digit_number_after_splitting_digits/minimum_sum_of_four_digit_number_after_splitting_digits.go
+++ go/minimum_sum_of_four_digit_number_after_splitting_digits/minimum_sum_of_four_digit_number_after_splitting_digits.go
@@ -1,26 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	fmt.Println(minimumSum(2932))
-	// fmt.Println(minimumSum(4009))
+	// fmt.Println(minimumSum(2932))
+	fmt.Println(minimumSum(4009))
 }
 
 func minimumSum(num int) int {
-	if num < 1000 && num > 9999 {
-		return -1
+	var arr []int
+	for num > 0 {
+		arr = append(arr, num%10)
+		num /= 10
 	}
 
-	i := 1
-	min := num
-	for i < 1000 {
-		i *= 10
-		res := (num / i) + (num % i)
-		fmt.Printf("%v + %v = %v\n", num/i, (num % i), res)
-		if res < min {
-			min = res
-		}
-	}
-	return min
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i] < arr[j]
+	})
+
+	return ((arr[0] * 10) + arr[2]) + (arr[1] * 10) + arr[3]
 }
