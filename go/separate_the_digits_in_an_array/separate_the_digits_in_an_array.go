bc5945eee98d8482aa5f94ee0e0e8756c546758e
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-03-10 09:38:27 +0700

    add solved solution for separate the digits in an array

--- go/separate_the_digits_in_an_array/separate_the_digits_in_an_array.go
+++ go/separate_the_digits_in_an_array/separate_the_digits_in_an_array.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(separateDigits([]int{113, 25, 83, 77}))
+	fmt.Println(separateDigits([]int{13, 25, 83, 77}))
 }
 
 func separateDigits(nums []int) []int {
 	var arr []int
 	for _, v := range nums {
-		fmt.Println(v / 10)
-		arr = append(arr, v)
+		var inarr []int
+		for v > 0 {
+			inarr = append([]int{v % 10}, inarr...)
+			v /= 10
+		}
+		arr = append(arr, inarr...)
 	}
-	return arr
-}
 
-func separate(num int) []int {
-	var arr []int
-	return append(arr)
+	return arr
 }
