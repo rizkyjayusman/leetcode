6d741f8a4dc5d2e025faec14cda5a095fb23debd
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-03-15 08:37:06 +0700

    add solved solution for maximum 69 number

--- go/maximum_69_number/maximum_69_number.go
+++ go/maximum_69_number/maximum_69_number.go
@@ -3,34 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	num := 9669
-	x := 1
-	for {
-		num /= 10
-		if num == 0 {
-			break
-		}
-		x *= 10
-	}
-
-	x *= num / x
-	fmt.Println(x)
-	// fmt.Println(maximum69Number(9669))
+	fmt.Println(maximum69Number(9669))
+	fmt.Println(maximum69Number(9996))
+	fmt.Println(maximum69Number(9999))
 }
 
 func maximum69Number(num int) int {
-	res := 0
-	for i := 1; i <= 4; i++ {
-		x := 1
-		for {
-			res += (num % 10) * x
-			num /= 10
-			x *= 10
-			fmt.Println(res)
-			if num == 0 {
-				break
-			}
+	max := num
+	x := 1
+	y := 1
+	for x < num {
+		x *= 10
+
+		r := num
+		d := (r % x) / y
+		add := 3 * y
+		if d == 6 {
+			r += add
+		} else if d == 9 {
+			r -= add
 		}
+
+		if r > max {
+			max = r
+		}
+
+		y *= 10
 	}
-	return res
+	return max
 }
