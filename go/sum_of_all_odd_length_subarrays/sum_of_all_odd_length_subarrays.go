ed9459bf0fe1ef781738f8059bc05c45a8d6c6ef
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-03-16 07:44:50 +0700

    add solved solution for sum of all odd length subarrays

--- go/sum_of_all_odd_length_subarrays/sum_of_all_odd_length_subarrays.go
+++ go/sum_of_all_odd_length_subarrays/sum_of_all_odd_length_subarrays.go
@@ -5,11 +5,31 @@ import (
 )
 
 func main() {
-	fmt.Println(sumOddLengthSubarrays([]int{1, 4, 2, 5, 3}))
-	// fmt.Println(sumOddLengthSubarrays([]int{1,2}))
-	// fmt.Println(sumOddLengthSubarrays([]int{10,11,12}))
+	// fmt.Println(sumOddLengthSubarrays([]int{1, 4, 2, 5, 3})) // 58
+	// fmt.Println(sumOddLengthSubarrays([]int{1, 2})) // 3
+	fmt.Println(sumOddLengthSubarrays([]int{10, 11, 12})) // 66
 }
 
 func sumOddLengthSubarrays(arr []int) int {
-	// TODO :: !!!!!oqijr0328jfeiwfwkoie
+	count := 0
+	odd := 1
+	for odd <= len(arr) {
+		strt := 0
+		x := 0
+		for x < len(arr) {
+			y := x
+			if y < len(arr) {
+				y += odd
+			}
+			for x < y && y <= len(arr) {
+				count += arr[x]
+				x++
+			}
+			strt++
+			x = strt
+		}
+		odd += 2
+	}
+
+	return count
 }
