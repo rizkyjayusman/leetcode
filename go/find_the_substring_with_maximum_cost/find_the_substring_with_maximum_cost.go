4532dfd33e027dacb67c2c40f25330e46edc3739
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-04-02 17:49:50 +0700

    [BIWEEKLY 101] fix and solve problem find the substring with maximum cost

--- go/find_the_substring_with_maximum_cost/find_the_substring_with_maximum_cost.go
+++ go/find_the_substring_with_maximum_cost/find_the_substring_with_maximum_cost.go
@@ -3,22 +3,28 @@ package main
 type FindTheSubstringWithMaximumCost struct{}
 
 func (ftswmc FindTheSubstringWithMaximumCost) MaximumCostSubstring(s string, chars string, vals []int) int {
-	res := 0
+	tmp, res := 0, 0
 	m := make(map[rune]int)
 	for k, v := range chars {
 		m[v] = vals[k]
 	}
 
 	for _, v := range s {
-		if tmp, ok := m[v]; ok {
-			res += tmp
-			if res < 0 {
-				res = 0
-			}
+		r := int(v - 96)
+		if rplc, ok := m[v]; ok {
+			r = rplc
+		}
+
+		x := tmp + r
+		if x < 0 {
+			tmp = 0
 			continue
 		}
-		res += int(v - 96)
 
+		tmp = x
+		if tmp > res {
+			res = tmp
+		}
 	}
 	return res
 }
